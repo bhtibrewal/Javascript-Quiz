d190540322cb86664a9d9fd03fa31d6ffe169048
Bhavika Tibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-28 17:46:03 +0000

    fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var readlineSync = require('readline-sync');
 
 var myAge = readlineSync.question('');
 
- var name = readlineSync.question('May I have your name? '+"\n");
- console.log('Hi ' + name + ' ! Welcome to my Quiz !!');
- console.log('Lets start the quiz...'+"\n");
+var name = readlineSync.question('May I have your name? ' + "\n");
+console.log('Hi ' + name + ' ! Welcome to my Quiz !!');
+console.log('Lets start the quiz...' + "\n");
 
 var score = 0;
 var highScores = [
@@ -19,42 +19,41 @@ var highScores = [
   },
 ]
 
-var questions = 
-[
-  {
-  question: `What will be the output of the below code ?
+var questions =
+  [
+    {
+      question: `What will be the output of the below code ?
   console.log( typeof( '5' + 5))
   `,
-  answer: "string"
-  }, 
-  {
-  question: `What will be the output of the following JavaScript code? 
+      answer: "string"
+    },
+    {
+      question: `What will be the output of the following JavaScript code? 
   int x=0;
   for(x;x<10;x++);
   console.log(x);
   `,
-  answer: "10"
-  },
-  {
-  question: `The fetch() method returns a ?
+      answer: "10"
+    },
+    {
+      question: `The fetch() method returns a ?
   `,
-  answer: "promise"
-  }
-];
+      answer: "promise"
+    }
+  ];
 
 
 
-function play(question, answer,i) {
-  var userAnswer = readlineSync.question((i+1)+" "+question);
+function play(question, answer, i) {
+  var userAnswer = readlineSync.question((i + 1) + " " + question);
 
-  if (userAnswer.toLowerCase() === answer.toLowerCase()) 
-  { 
+  if (userAnswer.toLowerCase() === answer.toLowerCase()) {
     console.log("right!");
     score = score + 1;
-    
+
   } else {
-    console.log("wrong!"+"\n"+"The answer is: " + answer);
-   
+    console.log("wrong!" + "\n" + "The answer is: " + answer);
+
   }
 
   console.log("current score: ", score);
@@ -62,19 +61,19 @@ function play(question, answer,i) {
 }
 
 function game() {
-  for (var i=0; i<questions.length; i++ ) {
+  for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
     play(currentQuestion.question, currentQuestion.answer, i)
     //calling function play reccursively till the loop ends
   }
 }
-function showScore(){
-console.log("You Scored: ", score +"/3");
-if (score===3){
-highScores.push({name,score})
-}
-console.log("Highest scorers :")
-highScores.map(score => console.log(score.name, " : ", score.score));
+function showScore() {
+  console.log("You Scored: ", score + "/3");
+  if (score === 3) {
+    highScores.push({ name, score })
+  }
+  console.log("Highest scorers :");
+  highScores.map(score => console.log(score.name, " : ", score.score));
 }
 
 game();
